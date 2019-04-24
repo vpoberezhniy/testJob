f991df07fa65d699457048e31a59e2c26bf5a07a
viktor-poberezhniy <viktor.poberezhniy@gmail.com>
2019-04-24 23:36:28 +0300

    Basic Laravel Auth: ability to log in as administrator

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('users')->insert([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password')]);
     }
 }
